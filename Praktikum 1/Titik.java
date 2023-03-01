2e04c38e46e4250adf9105a0c5e0d19e35386ce6
faradellaa <100354662+faradellaa@users.noreply.github.com>
2023-03-01 21:37:01 +0700

    Add files via upload

---
+++ Praktikum 1/Titik.java
@@ -0,0 +1,43 @@
+/**
+ * File		: Titik.java 22/02/2023
+ * Penulis	: Fara Della Anindia
+ * Deskripsi	: Kelas Titik
+**/
+
+class Titik{
+	double absis;
+	double ordinat;
+	static int counterTitik;
+	
+	Titik(){
+		absis = 0;
+		ordinat = 0;
+		counterTitik++;
+	}  
+	
+	Titik(double a, double o){
+		absis = a;
+		ordinat = o;
+		counterTitik++;
+	}
+  
+	void setAbsis(double a){
+		absis =a;
+	}
+  
+	double getAbsis(){
+		return absis;
+	}
+	
+	void setOrdinat(double b){
+		ordinat = b;
+	}
+	
+	double getOrdinat(){
+		return ordinat;
+	}
+	
+	int getCounterTitik(){
+		return counterTitik;
+	}
+}
