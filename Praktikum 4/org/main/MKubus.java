fd36f7fe9f678d3278044bad4666a23bf97109f1
faradellaa <100354662+faradellaa@users.noreply.github.com>
2023-03-15 19:48:56 +0700

    Add files via upload

---
+++ Praktikum 4/org/main/MKubus.java
@@ -0,0 +1,23 @@
+/**
+ * File : MKubus.java 15/03/2023
+ * Penulis : Fara Della Anindia
+ * Deskripsi : Main dari Kubus (praktikum 4)
+ */
+
+package org.main;
+
+import org.bangundatar.BujurSangkar;
+import org.bangunruang.Kubus;
+
+public class MKubus{
+	public static void main(String[] args){
+		BujurSangkar bujurSangkar = new BujurSangkar(4);
+		Kubus kubus = new Kubus(bujurSangkar);
+
+
+		System.out.println("Luas Alas Kubus dengan panjang sisi 4 satuan : "+kubus.hitungLuasAlas());
+		System.out.println("Volume Kubus dengan panjang sisi 4 satuan : "+kubus.hitungVolume());
+
+		
+	}
+}
