b19ec1946c6691c532f20139972dfe3152277390
faradellaa <100354662+faradellaa@users.noreply.github.com>
2023-05-17 19:57:05 +0700

    Add files via upload

--- Praktikum 8/BangunDatarGeneric.java
+++ Praktikum 8/BangunDatarGeneric.java
@@ -5,13 +5,17 @@
  */
 
 public class BangunDatarGeneric<T extends BangunDatar>{
+//public class BangunDatarGeneric<T1 extends BangunDatar>{
 	private T bangunDatar;
+	//private T1 bangunDatar;
 
 	public void set(T tipeBangunDatar){
+	//public void set(T1 tipeBangunDatar){
 		bangunDatar = tipeBangunDatar;
 	}	
 	
 	public T get(){
+	//public T1 get(){
 		return bangunDatar;
 	}
 
@@ -19,3 +23,7 @@ public class BangunDatarGeneric<T extends BangunDatar>{
 		return bangunDatar.hitungKeliling();
 	}
 }
+
+
+//saat T diganti T1, T2, ataupun yang lain tetap tidak masalah, tidak ada yang berubah
+//karena itu hanya penamaan dan T adalah standarisasi penamaannya
